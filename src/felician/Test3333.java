96959db25e08640d149c34f88bcd0da999996f1c
fjoksch <fjoksch@hotmail.com>
2017-03-16 10:58:30 +0100

    Version 2

--- src/felician/Test3333.java
+++ src/felician/Test3333.java
@@ -5,5 +5,7 @@ public class Test3333 {
 	public static void main(String[] args)
 	{
 		System.out.println("Guten Morgen Hr./Fr. Joksch!");
+		System.out.println("Guten Morgen Hr./Fr. Joksch!");
+		System.out.println("Guten Morgen Hr./Fr. Joksch!");
 	}
 }

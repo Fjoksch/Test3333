f78472b7d524a6bc0b5659e12a1cbb2eb7ed24db
fjoksch <fjoksch@hotmail.com>
2017-03-16 11:01:38 +0100

    smiley

--- src/felician/Test3333.java
+++ src/felician/Test3333.java
@@ -4,6 +4,6 @@ public class Test3333 {
 
 	public static void main(String[] args)
 	{
-		System.out.println("Guten Morgen Hr./Fr. Joksch!");
+		System.out.println("Guten Morgen Hr./Fr. Joksch :)");
 	}
 }
